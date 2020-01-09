c959eccff5e30692f97bd4fdff7d2a1e400fbf36
eliza tashbekova <tashbekovae@gmail.com>
2020-01-10 01:06:02 +0600

    Изменения в Model

--- EstelaVidaShop/Models/shop.Designer.cs
+++ EstelaVidaShop/Models/shop.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "C:\Users\user\source\repos\EstelaVidaShop\EstelaVidaShop\Models\shop.edmx" включено. 
+﻿// Создание кода T4 для модели "D:\telegram\EstelaVidaShop\EstelaVidaShop\Models\shop.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
